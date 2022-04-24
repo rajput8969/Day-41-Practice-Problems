e9abb74beb23fc367a82a12d94950397c99b0746
ANMOL SINGH RAJPUT <80009167+rajput8969@users.noreply.github.com>
2022-04-24 16:17:03 +0530

    Aarray in javascript

---
+++ Anmol/scrript.js
@@ -0,0 +1,39 @@
+let origDogs = ["Bulldog", "Beagle", "Labrador"]; 
+let cats = new Array("Americal Curl", "Bengal");
+let birds = new Array("Falcons", "Ducks", "Flamingoes");
+
+//Array Copy Elements
+
+let slicedDogs = origDogs.slice (1, 2);
+let copyDogs = [...origDogs];
+let dogs = origDogs.slice(0);
+
+//A Stack Functions (LIFO) Push and Pop
+
+let pushDog = dogs.push("Golden Retriever");
+let popDog = dogs.pop();
+dogs [dogs.length] = "Poodle";
+
+//Add and Remove from First
+
+let addfirst = dogs.unshift("Golden Retriever");
+let shiftDog = dogs.shift();
+
+// Atonic add and remove elements (where, how many to renove, element )
+
+dogs.splice(2, 1, "Pug", "Boxer");
+
+//Array Function - Concat, Slice and Sort
+
+let animals =dogs.concat(cats, birds); 
+let newAnimal =[ ...dogs, ...cats,...birds].toString();
+let sortDog = dogs.slice(0).sort();
+function scanArray ( [first, second]) { console.log("Scan: "+ first + " " + second);}
+scanArray(animals);
+let joinAnimals = animals.join(" ");
+let allAnimals = "";
+for (let animal of animals) allAnimals += animal + " " ;
+
+console.log("Animals :" +  allAnimals);
+
+
